a19e33e6a59138a36286c068cc14491d99702090
TangJjin <fygjbrdhvjj356@gmail.com>
2026-07-30 23:39:07 +0800

    你的提交说明

--- src/drone_bringup/scripts/start_d435.sh
+++ src/drone_bringup/scripts/start_d435.sh
@@ -7,9 +7,14 @@ source /opt/ros/humble/setup.bash
 source ~/drone_ws/install/setup.bash
 
 ros2 launch realsense2_camera rs_launch.py \
+  camera_namespace:=camera \
+  camera_name:=camera \
+  serial_no:=_327122074056 \
   enable_color:=true \
-  rgb_camera.color_profile:=640,480,30 \
-  enable_depth:=false \
-  enable_rgbd:=false \
-  enable_sync:=false \
-  align_depth.enable:=false
+  rgb_camera.color_profile:=640x480x30 \
+  rgb_camera.power_line_frequency:=1 \
+  enable_depth:=true \
+  depth_module.depth_profile:=640x480x30 \
+  enable_sync:=true \
+  enable_rgbd:=true \
+  align_depth.enable:=true
